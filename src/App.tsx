c340fa983b5cafe8c2f9fdc967ffcb4718ac53a0
David Olaoye <dtlaoye@gmail.com>
2024-10-07 08:03:32 -0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ToggleLogin from "./ToggleLogin";
+import Homepage from "./Homepage";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <ToggleLogin />
     </div>
   );
 }
